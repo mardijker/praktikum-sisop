4879460e598e293ef8dc467bb2c4e8dfbff45619
mardijker <33520837+mardijker@users.noreply.github.com>
2024-11-03 20:22:05 -0600

    Update wack-wick-weck.c

--- wack-wick-weck.c
+++ wack-wick-weck.c
@@ -4,8 +4,7 @@ void P1(void);
 void P2(void);
 void P3(void);
 
-// Deklarasi semaphores
-sid32 s_wick, s_weck, s_wack;
+sid32 s1, s2, s3;
 
 process main(void) {    
     // Membuat semaphores
@@ -14,14 +13,14 @@ process main(void) {
     s3 = semcreate(0);   // Wack dimulai dari 1 (allow to run first)
     
     // Membuat dan mengeksekusi proses
-    resume(create(wack, 1024, 20, "wack", 1, s1));
-    resume(create(wick, 1024, 20, "wick", 1, s2));
-    resume(create(weck, 1024, 20, "weck", 1, s3));
+    resume(create(P1, 1024, 20, "wack", 1, s1));
+    resume(create(P2, 1024, 20, "wick", 1, s2));
+    resume(create(P3, 1024, 20, "weck", 1, s3));
     
     return OK;
 }
 
-void wack(void) {
+void P1(void) {
     while (1) {
         wait(s1);
         printf("Wack\n");
@@ -30,7 +29,7 @@ void wack(void) {
     }
 }
 
-void wick(void) {
+void P2(void) {
     while (1) {
         wait(s2);
         printf("Wick\n");
@@ -39,7 +38,7 @@ void wick(void) {
     }
 }
 
-void weck(void) {
+void P3(void) {
     while (1) {
         wait(s3);
         printf("Weck\n");
